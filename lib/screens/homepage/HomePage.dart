80fac63cb6783d77ff7cb257c9e3304d30f8c739
tnhnyldz <tunahanyildiz1560@gmail.com>
2022-03-10 11:39:18 +0300

    scrollbar

--- lib/screens/homepage/HomePage.dart
+++ lib/screens/homepage/HomePage.dart
@@ -61,13 +61,17 @@ class _HomePageState extends State<HomePage> {
                     ),
                     SizedBox(
                       height: 300,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: _filmListFuture.length,
-                          itemBuilder: ((context, index) {
-                            MovieModel currentMovie = _filmListFuture[index];
-                            return HorizontalWidget(currentMovie: currentMovie);
-                          })),
+                      child: Scrollbar(
+                        showTrackOnHover: true,
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: _filmListFuture.length,
+                            itemBuilder: ((context, index) {
+                              MovieModel currentMovie = _filmListFuture[index];
+                              return HorizontalWidget(
+                                  currentMovie: currentMovie);
+                            })),
+                      ),
                     ),
                     Container(
                       padding: const EdgeInsets.all(4.0),
@@ -81,16 +85,20 @@ class _HomePageState extends State<HomePage> {
                             ),
                           )),
                     ),
-                    SizedBox(
-                      height: 300,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: _filmListTopRated.length,
-                          itemBuilder: ((context, index) {
-                            MovieModel currentModel2 = _filmListTopRated[index];
-                            return HorizontalWidget(
-                                currentMovie: currentModel2);
-                          })),
+                    Scrollbar(
+                      showTrackOnHover: true,
+                      child: SizedBox(
+                        height: 300,
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: _filmListTopRated.length,
+                            itemBuilder: ((context, index) {
+                              MovieModel currentModel2 =
+                                  _filmListTopRated[index];
+                              return HorizontalWidget(
+                                  currentMovie: currentModel2);
+                            })),
+                      ),
                     ),
                     Container(
                       padding: const EdgeInsets.all(4.0),
@@ -104,16 +112,21 @@ class _HomePageState extends State<HomePage> {
                             ),
                           )),
                     ),
-                    SizedBox(
-                      height: 300,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: _filmListFuture.length,
-                          itemBuilder: ((context, index) {
-                            MovieModel currentModel3 = _filmListTopRated[index];
-                            return HorizontalWidget(
-                                currentMovie: currentModel3);
-                          })),
+                    Scrollbar(
+                      // trackVisibility: true,
+                      showTrackOnHover: true,
+                      child: SizedBox(
+                        height: 300,
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: _filmListFuture.length,
+                            itemBuilder: ((context, index) {
+                              MovieModel currentModel3 =
+                                  _filmListTopRated[index];
+                              return HorizontalWidget(
+                                  currentMovie: currentModel3);
+                            })),
+                      ),
                     ),
                   ],
                 ),
