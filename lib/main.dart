af5499e6a98b109ddcc5a4231535f0d09aa21d16
Yıldıray Duran <duranyildirayy@gmail.com>
2022-03-22 17:12:57 +0300

    firebase eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:moviedb_api_app/firebase_options.dart';
 import 'package:moviedb_api_app/model/movie_model.dart';
 import 'package:moviedb_api_app/screens/main_page.dart';
 import 'package:moviedb_api_app/services/character_api.dart';
@@ -7,7 +9,11 @@ import 'package:moviedb_api_app/services/favorite_api.dart';
 
 import 'constants/consts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
